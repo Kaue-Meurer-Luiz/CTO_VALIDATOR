96ef7d153627ed7f3b2c936887a516dbb0fc1f23
Kaue Meurer Luiz <kauemluiz@gmail.com>
2025-09-30 01:18:10 -0300

    Implementando configuração CorsConfig

---
+++ ctoConference/src/main/java/br/com/infoservic/ctoConference/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package br.com.infoservic.ctoConference.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Aplica a todos os endpoints que começam com /api/
+                        .allowedOrigins("http://localhost:5173" ) // Permite requisições do seu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permite todos os cabeçalhos
+                        .allowCredentials(true); // Permite o envio de credenciais (cookies, cabeçalhos de autorização)
+            }
+        };
+    }
+}
